de08884c7ed072317faafc02d317ff3f4a4fbc30
“F1ReKing” <hyh9102@gmail.com>
2020-12-07 09:18:00 +0800

    Initial commit

---
+++ app/src/main/java/com/f1reking/oklog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.f1reking.oklog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
